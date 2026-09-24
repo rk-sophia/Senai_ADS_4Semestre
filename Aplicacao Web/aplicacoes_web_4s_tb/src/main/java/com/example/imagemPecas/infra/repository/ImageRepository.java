436401070390f91704278e341e01987ae4175565
Sophia RK <130664417+rk-sophia@users.noreply.github.com>
2026-09-24 19:29:07 -0300

    Adicionado projeto web

---
+++ Aplicacao Web/aplicacoes_web_4s_tb/src/main/java/com/example/imagemPecas/infra/repository/ImageRepository.java
@@ -0,0 +1,47 @@
+package com.example.imagemPecas.infra.repository;
+
+import com.example.imagemPecas.domain.entity.Image;
+import com.example.imagemPecas.domain.enums.ImageExtension;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+
+public interface ImageRepository extends JpaRepository<Image,String>, JpaSpecificationExecutor<Image> {
+
+    /**
+     *
+     * @param extension
+     * @param query
+     * @return
+     * SELECT * FROM IMAGE WHERE 1 = 1 AND EXTENSION = 'PNG' AND ( NAME LIKE 'QUERY'
+     * OR TAGS LIKE 'QUERY' )
+     */
+
+    default List<Image> findByExtensionAndNameOrTagsLike(ImageExtension extension, String query){
+        //SELECT * FROM IMAGE 1 = 1
+        Specification<Image> conjunction = (root, q, criteriaBuilder) -> criteriaBuilder.conjunction();
+        Specification<Image> spec = Specification.where(conjunction);
+
+        if (extension != null){
+           //AND EXTENSION = 'PNG'
+           Specification<Image> extensionEqual = (root, q, cb) -> cb.equal(root.get("extension"),extension);
+           spec = spec.and(extensionEqual);
+        }
+
+        if(StringUtils.hasText(query)){
+            //AND ( NAME LIKE 'QUERY' OR TAGS LIKE 'QUERY' )
+            Specification<Image> nameLike = (root, q, cb) -> cb.like( cb.upper(root.get("name")), "%" + query.toUpperCase() + "%");
+            Specification<Image> tagsLike = (root, q, cb) -> cb.like( cb.upper(root.get("tags")), "%" + query.toUpperCase() + "%");
+
+            Specification<Image> nameOrtagsLike = Specification.anyOf(nameLike, tagsLike);
+
+            spec = spec.and(nameOrtagsLike);
+        }
+
+        return findAll(spec);
+    }
+
+}
